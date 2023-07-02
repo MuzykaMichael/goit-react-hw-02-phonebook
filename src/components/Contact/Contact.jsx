729d920b8ff584a2ec3ef6771b8475fd63712aa0
Muzyka Mykhailo <Muzyka_Mishel@ukr.net>
2023-07-02 22:43:51 +0300

    Contact-markup+ContactList-markup

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,38 @@
+import { Component } from "react";
+import propTypes from 'prop-types'
+import {ContactLi,BtnDelete} from './Contact.styled'
+
+
+export class Contact extends Component{
+    deleteContact = id => {
+        this.props.handleDelete(id);
+    };
+
+
+render() {
+    const {name,number,id} = this.props;
+    return(
+        <ContactLi>
+            <p>
+                {name}:{number}
+            </p>
+            <BtnDelete
+            type="button"
+            onClick={()=>{
+                this.handleDelete(id);
+            }}
+            >Delete</BtnDelete>
+        </ContactLi>
+    )
+}
+
+
+}
+
+
+Contact.propTypes = {
+    id: propTypes.string.isRequired,
+    name:propTypes.string.isRequired,
+    number:propTypes.string.isRequired,
+    handleDelete:propTypes.func.isRequired,
+}
---
+++ src/components/Contact/Contact.styled.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+export const ContactLi = styled.li`
+
+`;
+
+export const BtnDelete = styled.button`
+
+`;
+
